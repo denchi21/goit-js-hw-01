3325c14c12976ea6ca60d4666ff7ff8c8ff4c45b
denni <dkurylenko21@gmail.com>
2025-03-09 22:05:56 +0100

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,9 @@ function getElementWidth(content, padding, border) {
     padding = (Number.parseFloat(padding)) * 2;
     border = (Number.parseFloat(border)) * 2;
     return (content + padding + border);
-    
+}
+
 
     console.log(getElementWidth("50px", "8px", "4px"));
     console.log(getElementWidth("60px", "12px", "8.5px"));
     console.log(getElementWidth("200px", "0px", "0px"));
-}
